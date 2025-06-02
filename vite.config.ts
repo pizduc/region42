e5fc1b939b27a20b3d275a89083ba54a9eea29b0
pizduc <74780707+pizduc@users.noreply.github.com>
2025-06-02 15:01:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";More actions
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-
   server: {
     host: "::",
     port: 8080,
@@ -19,12 +18,11 @@ export default defineConfig(({ mode }) => ({
   },
   preview: {
     port: 4173,
-    allowedHosts: ["region42.onrender.com", "best-yard.onrender.com"], // ✅ добавили Render-хосты
+    allowedHosts: ["region42.onrender.com", "best-yard.onrender.com"],
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
