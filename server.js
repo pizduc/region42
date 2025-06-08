4871da87c5625fd4df4ff87582053e90ad54e62d
pizduc <74780707+pizduc@users.noreply.github.com>
2025-06-08 21:34:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1032,7 +1032,7 @@ app.post("/api/email/send-receipt", async (req, res) => {
   }
 });
 
-const buildPath = path.resolve(__dirname, './dist');
+const buildPath = path.join(__dirname, 'build');
 
 app.use(express.static(buildPath));
 
